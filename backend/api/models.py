396f393bc6c56d9ac77928866ece20760e0883e8
Fellaine <max.goncharevych@gmail.com>
2023-05-05 18:32:04 +0300

    Added db index

--- backend/api/models.py
+++ backend/api/models.py
@@ -5,13 +5,13 @@ from django.contrib.auth.models import User
 User._meta.get_field('email')._unique = True
 
 
-# Create your models here.
-
 class Note(models.Model):
     title = models.CharField(max_length=128)
     content = models.TextField(null=True, blank=True)
     last_edited = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-
-    def __str__(self):
-        return self.title
+    
+    class Meta:
+        indexes = [
+            models.Index(fields=["user", "id"], include=["title", "content", "last_edited"], name="user_idx"),
+        ]
